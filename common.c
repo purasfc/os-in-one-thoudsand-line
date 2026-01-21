a67d10a1f087fbd368b755764bc3314ed397f07c
purasfc <pura@sfc.wide.ad.jp>
2026-01-21 15:05:10 +0100

    memory operations

--- common.c
+++ common.c
@@ -61,3 +61,18 @@ void printf(const char *fmt, ...) {
 end:
     va_end(vargs);
 }
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *)dst;
+    const uint8_t *s = (const uint8_t *)src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *)buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
