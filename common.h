94f2c8e1fbb4b73b9ae7dbf3931f5b424bbd4802
purasfc <pura@sfc.wide.ad.jp>
2026-01-21 16:56:15 +0100

    simplest memory allocation algorithm

--- common.h
+++ common.h
@@ -15,6 +15,7 @@ typedef uint32_t vaddr_t;
 #define align_up(value, align) __builtin_align_up(value, align)
 #define is_aligned(value, align) __builtin_is_aligned(value, align)
 #define offsetof(type, member) __builtin_offsetof(type, member)
+#define PAGE_SIZE 4096
 
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
