d04d2f031fc6fce3bddd7614da1ee805d1025244
purasfc <pura@sfc.wide.ad.jp>
2026-01-22 17:43:50 +0100

    building the application

--- run.sh
+++ run.sh
@@ -3,12 +3,18 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
 
 CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
+$CC $CFLAGS  -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
